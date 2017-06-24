69e3dcfbfcc6413580e10bccc6fbd2d58e6516ab
Eric <ericrainvillevt@gmail.com>
2017-06-23 20:21:40 -0400

    add classes

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * Created by eric on 6/23/17.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autocofigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
